e710e2958ea9cc92d342e130ae200f3de38ac595
jun022222222 <44199090+jun022222222@users.noreply.github.com>
2024-09-05 03:20:36 +0900

    Add files via upload

---
+++ Dependencies/Assets/Quacker.server.lua
@@ -0,0 +1,10 @@
+while true do
+	task.wait(math.random(5, 20))
+
+	if script.Parent ~= nil then
+		local name = `Quack{math.random(1, 4)}`
+		script.Parent:FindFirstChild(name):Play()
+	else
+		break
+	end
+end
